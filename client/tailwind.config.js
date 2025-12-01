37bffa81a33d30ebae538a4c4064047cd60b1ba9
MickPoletti <mason.mcdaniel.codes@gmail.com>
2025-12-01 00:51:26 +0000

    Initial commit for switch to vite

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
     extend: {
       fontFamily: {
@@ -10,10 +13,10 @@ module.exports = {
         "fallout-green": "#33DD7E",
       },
       backgroundImage: {
-        "game-table": "url('../public/png/tops_logo.png')",
-        "vault-boy": "url('assets/test.png')",
+        "game-table": "url('/png/tops_logo.png')",
+        "vault-boy": "url('/src/assets/test.png')",
       },
     },
   },
-  plugins: [require("tailwindcss"), require("autoprefixer")],
+  plugins: [],
 };
